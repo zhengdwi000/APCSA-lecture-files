8cea3208242ee64a5d89025775f56a067f5348b1
Dwight Zheng <herdorem000@mysbisd.org>
2025-09-02 13:39:19 -0500

    Update PS01_03.java

--- PS01_03.java
+++ PS01_03.java
@@ -1,1 +1,10 @@
-
+public class ProblemSetOneDotOne {
+  public static void main(String []args) {
+    System.out.println("Width: 4.5");
+    System.out.println("Height: 7.9");
+    System.out.print("Area: ");
+    System.out.println(4.5*7.9);
+    System.out.print("Perimeter:");
+    System.out.print((4.5+4.5)*(7.9+7.9));
+  }
+}
